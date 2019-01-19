acf927b588c0b94d8a553dc9b55c45fe129de111
Nicklas <nicklascschmidt@Nicklass-MacBook-Pro.local>
2019-01-18 21:41:14 -0800

    config update

--- config/config.js
+++ config/config.js
@@ -6,7 +6,6 @@ module.exports = {
     host: '127.0.0.1',
     dialect: 'mysql'
   },
-  OMDB_API_KEY: 'b3b5a67e',
   test: {
     username: 'root',
     password: '',
@@ -15,32 +14,18 @@ module.exports = {
     dialect: 'mysql'
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOSTNAME,
-    dialect: 'mysql',
-    use_env_variable: 'DATABASE_URL'
+    username: 'bd68e02d14da2f',
+    password: 'd3080578',
+    database: 'heroku_1bc712f3efe9b0f',
+    host: 'us-cdbr-iron-east-01.cleardb.net',
+    dialect: 'mysql'
   }
+  // production: {
+  //   username: process.env.DB_USERNAME,
+  //   password: process.env.DB_PASSWORD,
+  //   database: process.env.DB_NAME,
+  //   host: process.env.DB_HOSTNAME,
+  //   dialect: 'mysql',
+  //   use_env_variable: 'DATABASE_URL'
+  // }
 };
-
-
-
-// // INITIATE CONNECTION TO MYSQL
-
-// const Sequelize = require("sequelize");
-
-// // Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
-// const sequelize = new Sequelize("MovieMaster_db", "root", "", {
-//   host: "localhost",
-//   port: 3306,
-//   dialect: "mysql",
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     idle: 10000
-//   }
-// });
-
-// // Exports the connection for other files to use
-// module.exports = sequelize;
